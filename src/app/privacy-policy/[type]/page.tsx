10e89eb36e2e02968ca4b2cf065e0bec25a2f505
Vikash Roy <chiggos.vikash@gmail.com>
2024-11-18 22:00:28 +0530

    fix:type error in privacy policy

--- src/app/privacy-policy/[type]/page.tsx
+++ src/app/privacy-policy/[type]/page.tsx
@@ -1,22 +1,36 @@
-import Navbar from '@/components/home-page/navbar'
-import React from 'react'
+import React from 'react';
+import Navbar from '@/components/home-page/navbar';
+import { GetServerSideProps } from 'next';
 
-const PrivacyPolicy = ({type}:{type:string}) => {
-  return (
-    <main>
-    <Navbar 
-     type={type}
-     bgColor={"bg-primary"}/>
-    <section className='max-w-7xl mx-auto px-8 pt-24 flex flex-col items-center justify-center'>
-        <h1 className='text-5xl font-bold text-primary'>Privacy Policy</h1>
-        <div className='py-12  mt-8 bg-secondary rounded-lg shadow-md w-1/2 flex items-center justify-center'>
-            <h3 className='text-xl'>Coming Soon...</h3>
-
-        </div>
-      
-    </section>
-    </main>
-  )
+interface PrivacyPolicyProps {
+  type: string;
 }
 
-export default PrivacyPolicy
+const PrivacyPolicy: React.FC<PrivacyPolicyProps> = ({ type }) => {
+  return (
+    <main>
+      <Navbar 
+        type={type}
+        bgColor={"bg-primary"}
+      />
+      <section className='max-w-7xl mx-auto px-8 pt-24 flex flex-col items-center justify-center'>
+        <h1 className='text-5xl font-bold text-primary'>Privacy Policy</h1>
+        <div className='py-12 mt-8 bg-secondary rounded-lg shadow-md w-1/2 flex items-center justify-center'>
+          <h3 className='text-xl'>Coming Soon...</h3>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { type } = context.params as { type: string };
+
+  return {
+    props: {
+      type,
+    },
+  };
+};
+
+export default PrivacyPolicy;
