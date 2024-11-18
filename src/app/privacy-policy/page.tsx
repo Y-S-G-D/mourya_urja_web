b681245dd5dbc425cc586d9e8ef259f371026746
Vikash Roy <chiggos.vikash@gmail.com>
2024-11-18 22:12:26 +0530

    fix: error in policy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from '@/components/home-page/navbar'
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <main>
+    <Navbar 
+     bgColor={"bg-primary"}/>
+    <section className='max-w-7xl mx-auto px-8 pt-24 flex flex-col items-center justify-center'>
+        <h1 className='text-5xl font-bold text-primary'>Privacy Policy</h1>
+        <div className='py-12  mt-8 bg-secondary rounded-lg shadow-md w-1/2 flex items-center justify-center'>
+            <h3 className='text-xl'>Coming Soon...</h3>
+
+        </div>
+      
+    </section>
+    </main>
+  )
+}
+
+export default PrivacyPolicy
